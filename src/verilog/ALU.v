a13c115ed33a56f337c4841225323daa0571514f
Maricela <maricela.ramos.c@gmail.com>
2024-04-29 17:26:16 -0600

    Refactor: Se actualizan operaciones y bits de ALU de 4 a 3

--- src/verilog/ALU.v
+++ src/verilog/ALU.v
@@ -2,7 +2,7 @@
 module ALU(
 	input wire[31:0]op1,
 	input wire[31:0]op2,
-	input wire[3:0]sel,
+	input wire[2:0]sel,
 	output reg[31:0]res,
 	output ZF
 );
@@ -17,19 +17,20 @@ module ALU(
 always@(*)
 	begin
 	case(sel)
-	   4'b0000:
+	   3'b010: //AND
 	      ALU_Resultados = op1 & op2;
-	   4'b0001:
+	   3'b011: //OR
 	      ALU_Resultados = op1 | op2;
-	   4'b0010:
+	   3'b000: //ADD
 	      ALU_Resultados = op1 + op2;
-	   4'b0110:
+	   3'b001: //SUB
 	      ALU_Resultados = op1 - op2;
-	   4'b0111:
-	      ALU_Resultados = ~(op1 < op2);
-	   4'b1100:
+	   3'b101: //SLT
+	      ALU_Resultados = (op1 < op2) ? 1 : 0;
+	   3'b100: //NOR
 	      ALU_Resultados = ~(op1 | op2);
-	   default: ALU_Resultados = op1 - op2;
+	   default: 
+		  ALU_Resultados = 32'dx; // Pone el resultado como indeterminado
 	endcase
 end
 endmodule
--- src/verilog/ALU_Control.v
+++ src/verilog/ALU_Control.v
@@ -8,16 +8,17 @@ module ControladorALU (
 parameter ADD = 3'b000;
 parameter SUB = 3'b001;
 parameter AND = 3'b010;
-parameter OR = 3'b011;
-parameter XOR = 3'b100;
+parameter OR  = 3'b011;
+parameter NOR = 3'b100;
+parameter SLT = 3'b101;
 parameter NOP = 3'b111; // No operation (ninguna operación)
 
 // Definición de señales para operaciones R
 reg operacion_R;
 
 always @* begin
-    // Verifica si la instrucción es tipo R (asumiendo que el código de operación R es 3'b000000)
-    if (bits_instruccion == 6'b000000) begin
+    // Verifica si la instrucción es tipo R (asumiendo que el código de operación R es 3'b000)
+    if (codigo_UC == 3'b000) begin
         operacion_R = 1'b1; // La instrucción es tipo R
     end else begin
         operacion_R = 1'b0; // La instrucción no es tipo R
@@ -40,7 +41,8 @@ always @* begin
                     6'b100010: senial_ALU = SUB; // Si la instrucción es SUB
                     6'b100100: senial_ALU = AND; // Si la instrucción es AND
                     6'b100101: senial_ALU = OR; // Si la instrucción es OR
-                    6'b100110: senial_ALU = XOR; // Si la instrucción es XOR
+                    6'b100110: senial_ALU = NOR; // Si la instrucción es XOR
+					6'b101010: senial_ALU = SLT; //Si la instrucción es STL
                     default: senial_ALU = NOP; // Cualquier otro caso, no realiza operación
                 endcase
             end else begin // Si no es una instrucción tipo R
