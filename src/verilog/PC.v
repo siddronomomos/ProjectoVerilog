57b06bc9afeda65916e1fd785cab4b75fbd776d8
FerranST <167368320+FerranST@users.noreply.github.com>
2024-05-10 20:48:43 -0600

    Add files via upload

---
+++ src/verilog/PC.v
@@ -0,0 +1,25 @@
+`timescale 1ns/1ns
+//1. Declaracion modulos de salida
+module PC (
+    input [31:0] in_address,
+    output reg [31:0] out_address
+);
+//2. Definir elementos internos del modulo 
+
+reg clk;
+
+//3. Declarar los procesos secuenciales, asignaciones y/o instancias
+
+initial
+	begin
+		clk = 1'b0;
+		forever #100
+		clk=~clk;
+	end
+
+always@(posedge clk)
+    begin
+        out_address = in_address;
+    end
+
+endmodule
