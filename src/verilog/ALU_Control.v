2c8d083022ed6ef2ba3434eb05dfbe2a7acf8b49
FerranST <167368320+FerranST@users.noreply.github.com>
2024-05-19 22:40:50 -0600

    Update ALU_Control.v

--- src/verilog/ALU_Control.v
+++ src/verilog/ALU_Control.v
@@ -17,39 +17,29 @@ parameter NOP = 3'b111; // No operation (ninguna operacion)
 reg operacion_R;
 
 always @* begin
-    // Verifica si la instruccion es tipo R (asumiendo que el codigo de operacion R es 3'b000)
-    if (codigo_UC == 3'b000) begin
-        operacion_R = 1'b1; // La instruccion es tipo R
-    end else begin
-        operacion_R = 1'b0; // La instruccion no es tipo R
-    end
-
-    // Dependiendo del codigo de la UC y si la instruccion es tipo R, asigna la senial correspondiente para la ALU
-    case (codigo_UC)
+    if (codigo_UC == 3'b000) begin // Si es una operacion R
+        case (bits_instruccion)
+            6'b100000: senial_ALU = ADD; // Si la instruccion es ADD
+            6'b100010: senial_ALU = SUB; // Si la instruccion es SUB
+            6'b100100: senial_ALU = AND; // Si la instruccion es AND
+            6'b100101: senial_ALU = OR; // Si la instruccion es OR
+            6'b100110: senial_ALU = NOR; // Si la instruccion es XOR
+            6'b101010: senial_ALU = SLT; //Si la instruccion es STL
+            default: senial_ALU = NOP; // Cualquier otro caso, no realiza operacion
+        endcase
+    end else begin // Si no es una operacion R
+        case (codigo_UC)
         3'b000: senial_ALU = NOP; // No operation
-        3'b001: senial_ALU = NOP; // No operation
+        3'b001: senial_ALU = ADD; // No operation
         3'b010: senial_ALU = NOP; // No operation
         3'b011: senial_ALU = NOP; // No operation
         3'b100: senial_ALU = NOP; // No operation
         3'b101: senial_ALU = NOP; // No operation
         3'b110: senial_ALU = NOP; // No operation
-        3'b111: begin // Dependiendo de la instruccion tipo R o no, asigna la operacion correspondiente
-            if (operacion_R) begin // Si es una instruccion tipo R
-                // Asigna la senial de operacion de acuerdo a los bits 5:0 de la instruccion
-                case (bits_instruccion)
-                    6'b100000: senial_ALU = ADD; // Si la instruccion es ADD
-                    6'b100010: senial_ALU = SUB; // Si la instruccion es SUB
-                    6'b100100: senial_ALU = AND; // Si la instruccion es AND
-                    6'b100101: senial_ALU = OR; // Si la instruccion es OR
-                    6'b100110: senial_ALU = NOR; // Si la instruccion es XOR
-					6'b101010: senial_ALU = SLT; //Si la instruccion es STL
-                    default: senial_ALU = NOP; // Cualquier otro caso, no realiza operacion
-                endcase
-            end else begin // Si no es una instruccion tipo R
-                senial_ALU = NOP; // No operation
-            end
-        end
-    endcase
+        3'b111: senial_ALU = NOP; // No operation
+        default: senial_ALU = NOP; // Cualquier otro caso, no realiza operacion
+        endcase
+    end
 end
 
 endmodule
